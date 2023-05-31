63d3c9f9a4dfd4b630c97d1067c75833517979ff
Nitin <132347798+nitin8857888@users.noreply.github.com>
2023-06-01 00:32:47 +0530

    lifecycle hooks

--- src/components/Hello2.vue
+++ src/components/Hello2.vue
@@ -1,19 +1,36 @@
 <script setup>
 import { ref, computed } from "vue";
 // computed peoperty without ref
-let firstName = "nitin";
-let lastName = "mungilwar";
+// let firstName = "nitin";
+// let lastName = "mungilwar";
 
-const fullName = computed(() => {
-  return firstName + " " + lastName;
-});
+// const fullName = computed(() => {
+// console.log("getter called");
+//   return firstName + " " + lastName;
+// });
+
+// setTimeout(() => {
+// console.log("getter called");
+//   let firstName = "rekha";
+//   console.log("sttimeout called");
+// }, 3000);
+
+//computed peoperty with ref
+let firstName = ref("nitin");
+let lastName = ref("mungilwar");
+
+const fullName = () => {
+  console.log("getter called");
+  return firstName.value + " " + lastName.value;
+};
 
 setTimeout(() => {
-  let firstName = "rekha";
+  console.log("getter called");
+  firstName.value = "rekha";
   console.log("sttimeout called");
-}, 3000);
+}, 2000);
 </script>
 <template>
-  {{ fullName }}
+  {{ fullName() }}
 </template>
 <style></style>
